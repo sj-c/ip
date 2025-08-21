82c876bc17a57db8cf51f776ffd90bf52ff89e66
sinjie <sinjiechia123@gmail.com>
2025-08-21 16:21:39 +0800

    Level-1

--- src/main/java/Chatnius.java
+++ src/main/java/Chatnius.java
@@ -1,19 +1,69 @@
+import java.util.Scanner;
+
 public class Chatnius {
 
     public static String intro() {
         return "____________________________________________________________\n"
                 + " Hello! I'm Chatnius\n"
-                + " What can I do for you?\n"
+                + " What you need me for?\n"
                 + "____________________________________________________________\n";
     }
 
     public static String quit() {
-        return " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
+        return "____________________________________________________________\n" +
+                " Byebye! Use me again PLEASE\n" +
+                "____________________________________________________________\n";
+    }
+
+    public static String list() {
+        return "    ____________________________________________________________\n" +
+                "     list\n" +
+                "    ____________________________________________________________\n";
+    }
+
+    public static String blah() {
+        return "    ____________________________________________________________\n" +
+                "     blah\n" +
+                "    ____________________________________________________________\n";
     }
 
     public static void main(String[] args) {
-        System.out.print(Chatnius.intro());
-        System.out.print(Chatnius.quit());
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(intro());
+
+        boolean running = true;
+
+        while (running) {
+            System.out.print("What you need me for? ");
+            String input = scanner.nextLine().trim().toLowerCase();
+
+            switch (input) {
+                case "list":
+                    System.out.print(list());
+                    break;
+
+                case "blah":
+                    System.out.print(blah());
+                    break;
+
+                case "bye":
+                    System.out.print(quit());
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     I don't understand: " + input);
+                    System.out.println("    ____________________________________________________________");
+                    break;
+            }
+
+            if (running && !input.equals("bye")) {
+                System.out.print("What you need more for bro? ");
+            }
+        }
+
+        scanner.close();
     }
 }
