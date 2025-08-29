5a2b13424dc988a010c97b009f56aac9282ee410
sinjie <sinjiechia123@gmail.com>
2025-08-29 10:35:31 +0800

    Add gradle

--- src/main/java/DeleteCommand.java
+++
@@ -1,8 +0,0 @@
-public class DeleteCommand extends Command {
-    private final int idx;
-    public DeleteCommand(int idx) { this.idx = idx; }
-    @Override public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
-        ui.show(tasks.deleteMessage(idx));
-        storage.save(tasks);
-    }
-}
